fbf314863cac7e3dfe9f51c0806fed9415faab0b
Albertkatungu <albertkatungu@gmail.com>
2023-03-16 18:01:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#INCLUDE <stdio.h>
+/**
+ * main - Aprigram that prints a line using the printf junction
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
